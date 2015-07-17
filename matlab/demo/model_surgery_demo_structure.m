15241e17392a4c99a23b93b5c6dbb522585c060a
jiayuzhou <dearjiayu@gmail.com>
2015-07-17 13:46:55 -0700

    Fix a reference bug

--- matlab/demo/model_surgery_demo_structure.m
+++ matlab/demo/model_surgery_demo_structure.m
@@ -14,6 +14,9 @@ else
   error('Please run this demo from caffe/matlab/demo');
 end
 
+python_bin = '/usr/local/bin/python';
+setenv('DYLD_LIBRARY_PATH', '/usr/local/bin/'); % python lib path.
+
 addpath ../../../matlab-lmdb/  % change to your matlab-lmdb path
 cur_director = pwd;
 
@@ -37,7 +40,7 @@ test_func = @(network) mnist_test_lmdb( database, network, 3000, 0);
 [acc0, elps0 ] = test_func( net);
 fprintf('** The orignal network has the accuracy of %.4f with time cost %.4f sec.\n', acc0, elps0);
 
-%% Investigate model and test model surgery without structure change. 
+%% Investigate model and test model surgery without structure change.
 % investigate the ip1 level model parameter
 ip1_mat     = net.params('ip1',1).get_data;
 [U, S, V]   = svd(ip1_mat, 0);
@@ -63,7 +66,7 @@ fprintf('** The truncated network (without structure change) has the accuracy of
 % write the model out so you can use it later.
 %net.save('model_after_surgery.caffemodel')
 
-%% Model surgery with structure change. 
+%% Model surgery with structure change.
 % retain the weights (so we can later assign to a new structure)
 source_layer_name    = 'ip1';
 target_layer_names   = {'ip1a'; 'ip1b'};
@@ -71,7 +74,7 @@ param_ip1a = {U_r,         zeros(20, 1)};
 param_ip1b = {S_r * V_r',  net.params(source_layer_name,2).get_data};
 target_layer_params  = {param_ip1a, param_ip1b};
 
-% construct new parameter names and parameter storage. 
+% construct new parameter names and parameter storage.
 source_layer_id = net.name2layer_index(source_layer_name);
 net_layer_name = net.layer_names;
 
@@ -86,8 +89,8 @@ for i = 1: length(net_ip1rk20_layer_name)
     layer_name = net_ip1rk20_layer_name{i};
 
     if sum(strcmp(net.layer_names, layer_name))>0
-        % if the layers are in previous layers. 
-        
+        % if the layers are in previous layers.
+
         % explore how many blobs we need to store.
         layer_blobs_num = size(net.layer_vec(net.name2layer_index(layer_name)).params, 2);
 
@@ -97,10 +100,10 @@ for i = 1: length(net_ip1rk20_layer_name)
             layer_blobs{j} = net.params(layer_name, j).get_data;
         end
         net_ip1rk20_param{i} = layer_blobs;
-    
+
     elseif ~isempty(find(strcmp(target_layer_names, layer_name), 1))
-        % if the layers are newly inserted layers. 
-        
+        % if the layers are newly inserted layers.
+
         target_layer_idx = find(strcmp(target_layer_names, layer_name));
         net_ip1rk20_param{i} = target_layer_params{target_layer_idx};
     else
@@ -108,18 +111,18 @@ for i = 1: length(net_ip1rk20_layer_name)
     end
 end
 
-%%%% strategy 1: load model file if there is an exsiting one.  
+%%%% strategy 1: load model file if there is an exsiting one.
 % disp('Read new model file.')
 % net_ip1rk20_model   = strcat(cur_director, '/../../examples/mnist/lenet_ip1rk20.prototxt');
 % net_ip1rk20_weights = strcat(cur_director, '/../../examples/mnist/lenet_ip1rk20_iter_10000.caffemodel');
 % net = caffe.Net(net_ip1rk20_model, net_ip1rk20_weights, phase);
 
 % %%% strategy 2: use solver to instantiate an empty network with modified
-% %%% net prototxt 
-% net_ip1rk20_solver      = strcat(cur_director, '/lenet_ip1rk20_solver.prototxt'); 
+% %%% net prototxt
+% net_ip1rk20_solver      = strcat(cur_director, '/lenet_ip1rk20_solver.prototxt');
 % net_ip1rk20_model       = strcat(cur_director, '/lenet_ip1rk20.prototxt');
 % net_ip1rk20_init_weight = strcat(cur_director, '/lenet_ip1rk20_init.caffemodel');
-% 
+%
 % solver = caffe.Solver(net_ip1rk20_solver);
 % % cannot directly use the net from solver because it is at TRAIN phase
 % % and it seems caffe::set_phase is not available any more:
@@ -127,23 +130,21 @@ end
 % solver.net.save(net_ip1rk20_init_weight);
 % net = caffe.Net(net_ip1rk20_model, net_ip1rk20_init_weight, phase_test);
 
-%%% 
-net_ip1rk20_solver      = strcat(cur_director, '/lenet_ip1rk20_solver.prototxt'); 
+%%%
+net_ip1rk20_solver      = strcat(cur_director, '/lenet_ip1rk20_solver.prototxt');
 net_ip1rk20_model       = strcat(cur_director, '/lenet_ip1rk20_gen.prototxt');
-net_train_test          = strcat(cur_director, '/lenet.prototxt');
+net_train_test          = strcat(cur_director, '/lenet_train_test.prototxt');
 net_ip1rk20_train_test  = strcat(cur_director, '/lenet_ip1rk20_train_test_gen.prototxt');
 net_ip1rk20_init_weight = strcat(cur_director, '/lenet_ip1rk20_init.caffemodel');
 
-setenv('DYLD_LIBRARY_PATH', '/usr/local/bin/');
-
 disp('Transforming model structure file...')
-system(['/usr/local/bin/python netsurg_fclw.py ',source_layer_name, ...
+system([python_bin, ' netsurg_fclw.py ',source_layer_name, ...
             ' ', target_layer_names{1}, ' ' ,target_layer_names{2}, ...
             ' ', int2str(approx_rank),  ' ', net_model, ...
             ' 2>/dev/null 1> ', net_ip1rk20_model]);
 
 disp('Transforming model train/test file...')
-system(['/usr/local/bin/python netsurg_fclw.py ',source_layer_name, ...
+system([python_bin, ' netsurg_fclw.py ',source_layer_name, ...
             ' ', target_layer_names{1}, ' ' ,target_layer_names{2}, ...
             ' ', int2str(approx_rank),  ' ', net_train_test, ...
             ' 2>/dev/null 1> ', net_ip1rk20_train_test]);
@@ -154,19 +155,19 @@ solver.net.save(net_ip1rk20_init_weight);
 disp('Loading new model weight...')
 net = caffe.Net(net_ip1rk20_model, net_ip1rk20_init_weight, phase_test);
 
-% "fill in" the parameters layer by layer. 
+% "fill in" the parameters layer by layer.
 disp('Filling in new weights...')
 for i = 1: length(net_ip1rk20_layer_name)
     layer_name = net_ip1rk20_layer_name{i};
 
-    % check the consistency of the layer parameters. 
+    % check the consistency of the layer parameters.
     layer_blobs_num  = size(net.layer_vec(net.name2layer_index(layer_name)).params, 2);
     stored_blobs_num = length(net_ip1rk20_param{i});
     if(layer_blobs_num ~= stored_blobs_num)
         error('Blob number at layer [%s] (%d blobs) is inconsistently stored (%d blobs).', layer_name, layer_blobs_num, stored_blobs_num)
     end
-    
-    % set parameters. 
+
+    % set parameters.
     for j = 1: layer_blobs_num
         net.params(layer_name,j).set_data(net_ip1rk20_param{i}{j});
     end
